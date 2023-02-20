0d11483724833f13b0063fa59c8544a53e414b8b
ChogLight <sebas8812@gmail.com>
2023-02-19 22:18:39 -0500

    added icons and pokeball animation

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,35 +1,38 @@
 <script>
 // @ts-nocheck
-    import img from '../lib/images/pokeball.svg'
-    import home from '../lib/images/house-solid.svg'
-    import hammer from '../lib/images/hammer-solid.svg'
-    import phone from '../lib/images/mobile-retro-solid.svg'
-    import pen from '../lib/images/pen-nib-solid.svg'
-    import log from '../lib/images/right-to-bracket-solid.svg'
+    import Pokeball from '../lib/images/Pokeball.svelte'
     let navBarSize = "w-1/12"
+    let width = '60px'
+    let height = '60px'
+    let animation = '0'
     const changeIconHoverIn = () => {
         navBarSize = "w-1/6"
+        width = '120px'
+        height = '120px'
+        animation = 'rotate-[720deg]'
     }
     const changeIconHoverOut = () => {
         navBarSize = "w-1/12"
+        width = '60px'
+        height = '60px'
+        animation = "rotate-[-720deg]"
     }
 </script>
 
 <div on:mouseenter={changeIconHoverIn} on:mouseleave={changeIconHoverOut} 
-    class = "flex flex-col {navBarSize} bg-red-500 h-screen items-center text-white duration-300">
- 
+    class = "group/nav flex flex-col {navBarSize} bg-red-500 h-screen items-center text-white duration-300">
     <div class = "basis-1/3 p-6">
-        <a href="/"><img src={img} alt="logo"/></a>
+        <a href="/"><Pokeball width = {width} height = {height} animation = {animation}/></a>
     </div>
     <nav class="flex flex-col basis-2/3 text-2xl font-bold items-center">
         {#if navBarSize <= "w-1/6" && navBarSize> "w-1/12"}
             <a class="basis-1/3 hover:text-3xl duration-300" href="/">Home</a>
             <a class="basis-1/3 hover:text-3xl duration-300" href="/pokedex">PokeDex</a>
-            <a class="basis-1/3 hover:text-3xl duration-300" href="/teambuilder">Team builder</a>
+            <a class="text-xl basis-1/3 hover:text-3xl duration-300" href="/teambuilder">Team builder</a>
         {:else if navBarSize === "w-1/12"}
-            <a class="basis-1/3" href="/"><img height="20px" width="20px" src={home} alt="home"></a>
-            <a class="basis-1/3" href="/pokedex"><img height="15px" width="15px" src={phone} alt="dex"></a>
-            <a class="basis-1/3" href="/teambuilder"><img height="20px" width="20px" src={hammer} alt="build"></a>
+            <a class="basis-1/3" href="/"><i class="fa-solid fa-house"></i></a>
+            <a class="basis-1/3" href="/pokedex"><i class="fa-sharp fa-solid fa-mobile-retro"></i></a>
+            <a class="basis-1/3" href="/teambuilder"><i class="fa-solid fa-hammer"></i></a>
         {/if}
 
     </nav>
@@ -39,9 +42,8 @@
             <a class="hover:text-yellow-400 duration-100" href="/login">Log in</a>
             <a class = "hover:text-yellow-400 duration-100" href="/signin">Sign in</a>
         {:else if navBarSize == "w-1/12"}
-
-            <img height="20px" width="20px" src={log} alt="log">
-            <img height="20px" width="20px" src={pen} alt="pen">
+            <i class="fa-solid fa-right-to-bracket"></i>
+            <i class="fa-solid fa-pen-nib"></i>
         {/if}
         
     </div>
