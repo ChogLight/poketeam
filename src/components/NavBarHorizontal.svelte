7f3aa1d1bca24966e1656af95fd0abfd9a9618c9
ChogLight <sebas8812@gmail.com>
2023-02-20 23:09:28 -0500

    added media queries

---
+++ src/components/NavBarHorizontal.svelte
@@ -0,0 +1,22 @@
+<script>
+// @ts-nocheck
+    import Pokeball from '../lib/images/Pokeball.svelte'
+    export let navBarSize
+    export let animation
+    export let width
+    export let height
+</script>
+<nav class="flex justify-between {navBarSize} w-screen bg-red-500 items-center text-white duration-300 ">
+
+    <a href="/"><Pokeball width = {width} height = {height} animation = {animation}/></a>
+    <div class="flex gap-x-10 text-xl font-bold items-center">
+        <a class="basis-1/3" href="/"><i class="fa-solid fa-house"></i></a>
+        <a class="basis-1/3" href="/pokedex"><i class="fa-sharp fa-solid fa-mobile-retro"></i></a>
+        <a class="basis-1/3" href="/teambuilder"><i class="fa-solid fa-hammer"></i></a>
+    </div> 
+    <div class="flex items-center gap-x-5 mr-4">
+        <a class="hover:text-yellow-400 duration-100" href="/login"> <i class="fa-solid fa-right-to-bracket"></i></a>
+        <a class = "hover:text-yellow-400 duration-100" href="/signin"><i class="fa-solid fa-pen-nib"></i></a>
+    </div>
+
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,85 @@
 <script>
 // @ts-nocheck
+    import { onMount } from 'svelte'
+    import MediaQuery from '../components/MediaQuery.svelte';
+  	import NavBarVertical from '../components/NavBarVertical.svelte';
+    import NavBarHorizontal from '../components/NavBarHorizontal.svelte';
+    import '../app.css';
 
-  	import NavBar from '../components/NavBar.svelte';
-	import '../app.css'
-	let navBarSize = 'basis-1/12'
-	let animation
-	let pageSize = 'basis-11/12'
-    let width = '60px'
-    let height = '60px'
+    let animation
+	let navBarSize
+	let pageSize
+    let width
+    let height
+    let orientation
     const changeIconHoverIn = () => {
-        navBarSize = "basis-1/6"
-		pageSize = 'basis-5/6'
-        width = '120px'
-        height = '120px'
-        animation = 'rotate-[720deg]'
+        if(window.innerWidth > 500){
+            navBarSize = "basis-1/6"
+		    pageSize = 'basis-5/6'
+            width = '120px'
+            height = '120px'
+            animation = 'rotate-[720deg]'
+        }
+        else{
+            animation = 'rotate-[720deg]'
+        }
+        
     }
     const changeIconHoverOut = () => {
-        navBarSize = "basis-1/12"
-        width = '60px'
-        height = '60px'
-        animation = "rotate-[-720deg]"
-		pageSize = 'basis-11/12'
+
+        if(window.innerWidth > 500){
+            navBarSize = "basis-1/12"
+            width = '60px'
+            height = '60px'
+            animation = "rotate-[-720deg]"
+            pageSize = 'basis-11/12'
+        }
+        else{
+            navBarSize = "basis-1/12"
+            width = '60px'
+            height = '60px'
+            animation = "rotate-[-720deg]"
+            pageSize = 'basis-11/12'
+        }
     }
+
+    onMount(() => {
+        if(window.innerWidth > 500){
+	        navBarSize = 'basis-1/12'
+	        pageSize = 'basis-11/12'
+            width = '60px'
+            height = '60px'
+            orientation = ''
+        }
+        else{
+            animation
+	        pageSize = ''
+            orientation = 'flex-col'
+            navBarSize = 'basis-1/12'
+            pageSize = 'basis-11/12'
+            width = '60px'
+            height = '60px'
+        }
+    })
 </script>
 
-<div class="app font-mono flex">
-	<div on:mouseenter={changeIconHoverIn} on:mouseleave={changeIconHoverOut} class="{navBarSize} duration-300">
-		<NavBar navBarSize = {navBarSize} width = {width} height = {height} animation = {animation}/>
-	</div>
+<div class="app font-mono flex {orientation}">
+
+    <MediaQuery query="(min-width: 1281px)" let:matches>
+        {#if matches}
+            <div on:mouseenter={changeIconHoverIn} on:mouseleave={changeIconHoverOut} class="{navBarSize} duration-300">
+                <NavBarVertical navBarSize = {navBarSize} width = {width} height = {height} animation = {animation}/>
+            </div>
+        {/if}
+    </MediaQuery>
+
+    <MediaQuery query = "(max-width: 428px)" let:matches>
+        {#if matches}
+            <div on:mouseenter={changeIconHoverIn} on:mouseleave={changeIconHoverOut} class="{navBarSize} duration-300">
+                <NavBarHorizontal navBarSize={navBarSize} width = {width} heigth = {height} animation = {animation}/>
+            </div>
+        {/if}
+    </MediaQuery>
 	<main class= "{pageSize} duration-300">
 		<slot />
 	</main>
